f462f1141d4186be61925e929e54a8e004963405
zulu <core@didan.org>
2021-01-12 14:43:24 +0300

    split
    split

--- test/unit/conftest.py
+++ test/unit/conftest.py
@@ -1,12 +1,12 @@
 """Test fixtures."""
 from builtins import super
 
-import pytest
 from napalm.base.test import conftest as parent_conftest
-
 from napalm.base.test.double import BaseTestDouble
 
-from napalm_skeleton import skeleton
+from napalm_ftos import ftos
+
+import pytest
 
 
 @pytest.fixture(scope='class')
@@ -16,9 +16,9 @@ def set_device_parameters(request):
         request.cls.device.close()
     request.addfinalizer(fin)
 
-    request.cls.driver = skeleton.SkeletonDriver
-    request.cls.patched_driver = PatchedSkeletonDriver
-    request.cls.vendor = 'skeleton'
+    request.cls.driver = ftos.FTOSDriver
+    request.cls.patched_driver = PatchedFTOSDriver
+    request.cls.vendor = 'ftos'
     parent_conftest.set_device_parameters(request)
 
 
@@ -27,19 +27,38 @@ def pytest_generate_tests(metafunc):
     parent_conftest.pytest_generate_tests(metafunc, __file__)
 
 
-class PatchedSkeletonDriver(skeleton.SkeletonDriver):
-    """Patched Skeleton Driver."""
+class PatchedFTOSDriver(ftos.FTOSDriver):
+    """Patched FTOS Driver."""
 
     def __init__(self, hostname, username, password, timeout=60, optional_args=None):
-        """Patched Skeleton Driver constructor."""
+        """Patched FTOS Driver constructor."""
         super().__init__(hostname, username, password, timeout, optional_args)
 
         self.patched_attrs = ['device']
-        self.device = FakeSkeletonDevice()
+        self.device = FakeFTOSDevice()
+
+    def open(self):
+        """Fake driver, don't do anything."""
+        pass
+
+    def disconnect(self):
+        """Fake driver, don't do anything."""
+        pass
 
 
-class FakeSkeletonDevice(BaseTestDouble):
-    """Skeleton device test double."""
+class FakeFTOSDevice(BaseTestDouble):
+    """FTOS device test double."""
+
+    def send_command(self, command, **kwargs):
+        """Fake driver, get output from file."""
+        filename = '{}.txt'.format(self.sanitize_text(command))
+        full_path = self.find_file(filename)
+        result = self.read_txt_file(full_path)
+        return str(result)
+
+    def disconnect(self):
+        """Fake driver, don't do anything."""
+        pass
 
     def run_commands(self, command_list, encoding='json'):
         """Fake run_commands."""
